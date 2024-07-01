7bf54d189f4f9e331a18928dc6777be47a86d916
Rafael Estrada <rafaelestrada@comcast.net>
2024-07-01 10:54:27 -0700

    first commit

---
+++ jobportal/src/main/java/com/luv2code/jobportal/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.luv2code.jobportal.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    public String home() {
+        return "index";
+    }
+}
